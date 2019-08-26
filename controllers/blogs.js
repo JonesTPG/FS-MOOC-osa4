1f798d208841e4a1af1a4df3591eaf1d2df28a75
JonesTPG <joonas.ryynanen1@gmail.com>
2019-08-26 20:20:04 +0300

    4.8-4.12 tehty.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,20 +4,26 @@ const Blog = require('../models/blog');
 blogsRouter.get('/', (request, response, next) => {
   Blog.find({})
     .then(blogs => {
-      response.json(blogs);
+      response.json(blogs.map(blog => blog.toJSON()));
     })
     .catch(error => next(error));
 });
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    })
-    .catch(error => next(error));
+  if (blog.likes == undefined) {
+    blog.likes = 0;
+  }
+  if (blog.title == undefined || blog.url == undefined) {
+    response.status(400).send({ error: 'malformatted blog' });
+    return;
+  }
+  try {
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog.toJSON());
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = blogsRouter;
---
+++ tests/blog_api.test.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const _ = require('lodash');
+
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: 'title1',
+    author: 'adsf',
+    url: 'fdf-tgdfgiimi.fi',
+    likes: 5
+  },
+  {
+    title: 'title2',
+    author: 'adsf',
+    url: 'fdf-tgdfgiimi.fi',
+    likes: 15
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('identifying field is id instead of _id', async () => {
+  const response = await api.get('/api/blogs');
+  _.forEach(response.body, blog => {
+    expect(blog.id).toBeDefined();
+  });
+});
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'new blog',
+    author: 'fdsa',
+    url: 'fdf-tfgiimi.fi',
+    likes: 3
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+
+  const titles = response.body.map(r => r.title);
+
+  expect(response.body.length).toBe(initialBlogs.length + 1);
+  expect(titles).toContain('new blog');
+});
+
+test('if likes is undefined set zero likes', async () => {
+  const newBlog = {
+    title: 'new blog without likes',
+    author: 'sdfdfgdfg',
+    url: 'fdf-adfdf.fi'
+  };
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  expect(response.body.likes).toBe(0);
+});
+
+test('blog without title and url is not added', async () => {
+  const newBlog = {
+    author: 'sdfssdf',
+    likes: 3
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400);
+
+  const response = await api.get('/api/blogs');
+  expect(response.body.length).toBe(initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
