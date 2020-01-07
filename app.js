17dd2176a7d466394dc973447d22d8d21ba1757d
JonesTPG <joonas.ryynanen1@gmail.com>
2020-01-07 20:34:56 +0200

    7.17-7.18 tehty.

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/test');
+  app.use('/api/testing', testingRouter);
+}
+
 app.use(errorHandler.err);
 
 module.exports = app;
